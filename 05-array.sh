f3ea68697e8294376f2b3b0db56c7f42be57fe50
ktharuni <kompallytharuni@gmail.com>
2024-10-08 15:08:29 +0530

    array

---
+++ 05-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Fruits=("apple","orange","kiwi")#array
+  
+echo "First fruit is ${Fruits[0]}"
+echo "second fruit is ${Fruits[1]}"
+echo "Third fruit is ${Fruits[2]}"
+
+echo "all fruits are ${Fruits[@]}"
