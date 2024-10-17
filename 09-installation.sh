dd52c4ffe4730b2739e113d2513e86317c9a5658
ktharuni <kompallytharuni@gmail.com>
2024-10-17 12:31:52 +0530

    git installation

--- 09-installation.sh
+++ 09-installation.sh
@@ -8,4 +8,12 @@ then
     exit 1
 fi
 
+dnf list installed git
+
+if [ $? -ne 0]
+then 
+echo "Git is not installed...going to install it"
 dnf install git -y
+else 
+echo "Git is already installed"
+fi
