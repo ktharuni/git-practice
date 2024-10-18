d5435aa6fc751788c9296d0bdee2432eb2f57fd0
ktharuni <kompallytharuni@gmail.com>
2024-10-18 13:26:31 +0530

    redirector the output in a file

--- 14-redirectors.sh
+++ 14-redirectors.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(echo $0 | cut -d "." f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
 mkdir -p $LOGS_FOLDER
