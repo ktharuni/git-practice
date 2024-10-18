e72addf8564f8b08d64437f52b3b817936c9611d
ktharuni <kompallytharuni@gmail.com>
2024-10-18 13:00:14 +0530

    redirector the output in a file

---
+++ 14-redirectors.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]
+then 
+    echo "$R Please run this script with root privileges $N" &>> $LOG_FILE
+    exit 1
+fi
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R failed $N" &>> $LOG_FILE
+        exit 1
+    else  
+       echo -e "$2 is $G successful $N" &>> $LOG_FILE
+    fi
+}
+
+for package in $@
+do
+dnf list installed $package &>> $LOG_FILE 
+if [ $? -ne 0 ]
+then 
+echo -e "$Y $package is not installed...going to install it $N" &>> $LOG_FILE
+dnf install $package -y
+VALIDATE $? "Installing $package" 
+else 
+echo -e "$Y $package is already installed $N" &>> $LOG_FILE
+fi
+done
