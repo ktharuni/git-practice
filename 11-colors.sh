e9f8df75c2fa5f3b06c870bca50247d700a1dd9c
ktharuni <kompallytharuni@gmail.com>
2024-10-18 11:12:13 +0530

    adding colours

---
+++ 11-colors.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[32m"
+G="\e[33m"
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run this script with root privileges"
+    exit 1
+fi
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is $R failed $N"
+        exit 1
+    else  
+       echo "$2 is $G successful $N"
+    fi
+}
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then 
+echo "Git is not installed...going to install it"
+dnf install git -y
+VALIDATE $? "Installing git"
+else 
+echo "Git is already installed"
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    echo "mysql is not installed...going to install it"
+    dnf install mysql -y
+    VALIDATE $? "Installing mysql"
+else 
+echo "mysql is already installed"
+fi
