6ef57d5ad492dd48f98f310f3517a7c59e6fc02f
ktharuni <kompallytharuni@gmail.com>
2024-10-18 11:57:05 +0530

    installing packages using loop

---
+++ 13-loops.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run this script with root privileges"
+    exit 1
+fi
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is failed"
+        exit 1
+    else  
+       echo "$2 is successful"
+    fi
+}
+
+for package in $@
+do
+dnf list installed $package
+if [ $? -ne 0 ]
+then 
+echo "$package is not installed...going to install it"
+dnf install $package -y
+VALIDATE $? "Installing $package"
+else 
+echo "$package is already installed"
+fi
+done
