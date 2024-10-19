c543b64962207303dcbf9b7f59ea02a2f4250d8e
ktharuni <kompallytharuni@gmail.com>
2024-10-19 17:26:26 +0530

    set command usage

--- 15-set-command.sh
+++ 15-set-command.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 set -e
+failure(){
+   echo "Failed at : $1:$2" 
+}
+
+trap 'failure "${LINENO}" "${BASH_COMMAND}"' ERR
 echo "klojmo,lh,tlyjmmnm"
 
 echooo ",gbl,bot,myoolh,l,hl,hl"
