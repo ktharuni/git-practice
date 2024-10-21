2defe753407365461f73d4f17bc46b4a7b49cd39
ktharuni <kompallytharuni@gmail.com>
2024-10-21 13:16:01 +0530

    shell

--- 19-frequency-words.sh
+++ 19-frequency-words.sh
@@ -8,6 +8,6 @@ N="\e[0m"
  
 while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
-  NUM=$($line| wc -l)
+  NUM=$($line | wc -l)
   echo "$NUM"
 done <<< $SOURCE_FILE
