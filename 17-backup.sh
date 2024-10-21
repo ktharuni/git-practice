803178385f6d128b54ebecda0b578ff4af4e55d6
ktharuni <kompallytharuni@gmail.com>
2024-10-21 09:56:03 +0530

    backup

---
+++ 17-backup.sh
@@ -0,0 +1,38 @@
+!#/bin/bash
+
+SOURCE_DIR=$1
+DESTINATION_DIR=$2
+DAYS=[[$3:-14]]
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+USAGE
+{
+    echo -e "$R Usage : $N sh backup.sh <source dir> <destination dir> <days(optional)>
+}
+
+if [ $# -lt 2 ]
+then
+    USAGE
+    exit 1
+fi
+
+if [ ! -d $SOURCE_DIR ]
+then
+    echo "source directory does not exists"
+fi
+if [ ! -d $DESTINATION_DIR ]
+then
+    echo "destination directory does not exists"
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+
+if [ -n $FILES ]
+then 
+    echo "files found"
+else
+    echo "files does not found which are older than $DAYS"
+    exit 1
+fi
