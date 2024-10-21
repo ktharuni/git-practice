5de55cd25b63a8e103d402a915588ae41499b2bb
ktharuni <kompallytharuni@gmail.com>
2024-10-21 10:40:09 +0530

    backup

--- 17-backup.sh
+++ 17-backup.sh
@@ -1,37 +1,60 @@
 #!/bin/bash
 
 SOURCE_DIR=$1
-DESTINATION_DIR=$2
-DAYS=${3:-14}
+DEST_DIR=${2}
+DAYS=${3:-14} #if $3 is empty, default is 14 days.
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
-USAGE()
-{
-    echo -e "$R Usage : $N sh backup.sh <source dir> <destination dir> <days(optional)>
+USAGE(){
+    echo -e "$R USAGE:: $N sh 19-backup.sh <source> <destination> <days(optional)>"
 }
+#check the source and destination are provided
 
 if [ $# -lt 2 ]
 then
     USAGE
+    exit 1
 fi
 
 if [ ! -d $SOURCE_DIR ]
 then
-    echo "source directory does not exists"
+    echo "$SOURCE_DIR does not exist...Please check"
 fi
-if [ ! -d $DESTINATION_DIR ]
+
+if [ ! -d $DEST_DIR ]
 then
-    echo "destination directory does not exists"
+    echo "$DEST_DIR does not exist...Please check"
 fi
 
-FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
-echo "files:$FILES" 
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS)
 
-if [ ! -z $FILES ]
-then 
-   echo "files found"
+echo "Files: $FILES"
+
+if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
+then
+    echo "Files are found"
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS | zip "$ZIP_FILE" -@
+
+    #check if zip file is successfully created or not
+    if [ -f $ZIP_FILE ]
+    then
+        echo "Successfully zippped files older than $DAYS"
+        #remove the files after zipping
+        while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+        do
+            echo "Deleting file: $file"
+            rm -rf $file
+        done <<< $FILES
+    else
+        echo "Zipping the files is failed"
+        exit 1
+    fi
 else
-    echo "files does not found which are older than $DAYS"
+    echo "No files older than $DAYS"
 fi
