aca3a75c465c35a107c6ed18a21176ea30b277ec
ktharuni <kompallytharuni@gmail.com>
2024-10-21 13:06:12 +0530

    shell

--- 19-frequency-words.sh
+++ 19-frequency-words.sh
@@ -8,18 +8,6 @@ N="\e[0m"
 
 while IFS= read -r line
 do 
-   for i in {1...N}
-   do
-   WORD=$(awk -F " " '{print $($i)F}')
-   for j in {1...N}
-   do
-   COMPARE_WORDS=$(awk -F " " '{print $($j)F}')
-   if [ WORD -et COMPARE_WORDS ]
-   then
-   COUNT=$COUNT+1
-   fi
-   done
-   COUNT=$COUNT-1
-   echo -e "$G $WORD $N : $R $COUNT $N"
-   done
+  NUM=$(grep -i $line | wc -l)
+  echo "$NUM"
 done <<< $SOURCE_FILE
