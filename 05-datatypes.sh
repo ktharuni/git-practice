091acab0864f93802f151f1bd9dd9731daf120d3
ktharuni <kompallytharuni@gmail.com>
2025-02-09 15:49:05 +0530

    arrayorarraylist datatype

---
+++ 05-datatypes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+CHOCOLATES=("KITKAT" "DIARYMILK" "5STAR")#arrayorarraylist
+
+echo "Buy me a $CHOCOLATES[0]"
+echo "I like $CHOCOLATES[1]"
+echo "It's ok to manage with $CHOCOLATES[2]"
+echo "I want to buy $CHOCOLATES[@]"
