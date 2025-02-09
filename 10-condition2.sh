257a28bb6bcc5484936ec403e5f78fecb8325714
ktharuni <kompallytharuni@gmail.com>
2025-02-09 16:33:17 +0530

    mysql package installation

---
+++ 10-condition2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+if [ $? -ne 0 ]
+then
+    echo "Please check root access privileges"
+    exit 1
+fi
+
+dnf list installed mysql-server
+
+if [ $? -ne 0 ]
+then
+    echo "You are not yet installed...installing...."
+    dnf install mysql-server -y
+    if [ $? -ne 0 ]
+    then
+        echo "mysql installation is failed"
+    else
+        echo "mysql installation is successful"
+    fi
+else
+    echo "mysql already installed"
+fi
