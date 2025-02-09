db10ce4321f4f1378f971de58805f427e3ff514c
ktharuni <kompallytharuni@gmail.com>
2025-02-09 15:31:45 +0530

    variables inside script

---
+++ 02-variable1.sh
@@ -0,0 +1,18 @@
+!#/bin/bash
+
+PERSON1=Ram
+PERSON2=Raju
+
+echo "$PERSON1: Hey ${PERSON2}, how’s it going?"
+
+echo "${PERSON2}: Good, $PERSON1! Been busy with work. What about you?"
+
+echo "$PERSON1: Same here, but I went hiking last weekend. It was amazing!"
+
+echo "${PERSON2}: That sounds awesome! Where did you go?"
+
+echo "$PERSON1: Sundar Hills. You should come next time!"
+
+echo "${PERSON2}: I’m in! Let’s plan it."
+
+echo "$PERSON1: Great, I’ll check my schedule and let you know."
