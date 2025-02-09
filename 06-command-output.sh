3eb06c4d3f25445f5b6736b516b024465352ca94
ktharuni <kompallytharuni@gmail.com>
2025-02-09 16:01:43 +0530

    updated 2 ways to execute command

--- 06-command-output.sh
+++ 06-command-output.sh
@@ -4,4 +4,4 @@ date +%F #direct command executed
 
 DATE=$(date +%F) #command executed by shell and output stored in variable DATE at run time
 
-Script started execution at $DATE
+echo "Script started execution at $DATE"
